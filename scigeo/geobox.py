9c4663960e7ec687a6360e250eb2aa433f473523
Songyan Zhu <zhusy93@gmail.com>
2022-09-07 16:02:59 +0100

    Update geobox.py

--- scigeo/geobox.py
+++ scigeo/geobox.py
@@ -325,3 +325,23 @@ def get_zonal_stats(shp, arr2D, minlon, minlat, maxlon, maxlat, width, height):
     affine = rio.transform.from_bounds(minlon, minlat, maxlon, maxlat, width, height)
     stat_vals = zonal_stats(shp, arr2D, affine=affine, stats=['min', 'max', 'mean', 'median', 'majority', 'sum'])[0]
     return stat_vals
+
+def split_roi(minlon, maxlon, minlat, maxlat, nrow, ncol):
+    lons = np.linspace(minlon, maxlon, ncol)
+    lats = np.linspace(minlat, maxlat, nrow)
+    lons, lats = np.meshgrid(lons, lats)
+
+    sub_coords = []
+    for i in range(nrow - 1):
+        for j in range(ncol - 1):
+            rect_lons = [
+                lons[i, j], lons[i, j + 1],
+                lons[i + 1, j + 1], lons[i + 1, j]
+            ]
+            rect_lats = [
+                lats[i, j], lats[i, j + 1],
+                lats[i + 1, j + 1], lats[i + 1, j]
+            ]
+            pairs = [list(pair) for pair in zip(rect_lons, rect_lats)]
+            sub_coords.append(pairs)
+    return sub_coords
