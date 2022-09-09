fa112a6004c1d6bb5c0d00c3f3a67558ef7f2090
Songyan Zhu <zhusy93@gmail.com>
2022-09-09 11:33:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "scigeo",
-	version = "0.0.12",
+	version = "0.0.13",
 	keywords = ("easy geo warpper"),
 	description = "read/dump/process geo data",
 	long_description = "coming soon",
