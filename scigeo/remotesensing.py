b2907400de70a5121f1347aa7a379e184a4d76c2
Songyan Zhu <zhusy93@gmail.com>
2025-04-19 08:06:31 +0100

    Update remotesensing.py

--- scigeo/remotesensing.py
+++ scigeo/remotesensing.py
@@ -41,3 +41,50 @@ def deg2m(longitude, latitude, scale_lon, scale_lat):
     coef_mat = 40075 * np.cos(np.deg2rad(np.abs(lats))) / 360 * 111.32 * 1e3 * 1e3
     coef_mat = coef_mat * scale_lon * scale_lat
     return coef_mat
+
+
+
+class VegIdx:
+    def __init__(self):
+      pass
+    # NDVI
+    # --------------------------------------------------------------------------------------------------------------
+    @staticmethod
+    def modis_ndvi(b1, b2):
+        return (b2 - b1) / (b2 + b1)
+
+    @staticmethod
+    def landsat7_ndvi(b3, b4):
+        return (b4 - b3) / (b4 + b3)
+
+    @staticmethod
+    def landsat8_ndvi(b4, b5):
+        return (b5 - b4) / (b5 + b4)
+
+    @staticmethod
+    def sentinel2_ndvi(b8, b4):
+        return (b8 - b4) / (b8 + b4)
+
+    # EVI
+    # --------------------------------------------------------------------------------------------------------------
+    # EVI = G * ((NIR - R) / (NIR + C1 * R - C2 * B + L))
+    @staticmethod
+    def modis_evi_2band(b1, b2):
+        return 2.5 * ((b2 - b1) / (b2 + 2.4 * b1 + 1))
+
+    @staticmethod
+    def modis_evi_3band(b1, b2, b3):
+        return 2.5 * ((b2 - b1) / (b2 + 6 * b1 - 7.5 * b3 + 1))
+
+    @staticmethod
+    def landsat7_evi(b1, b3, b4):
+        return 2.5 * ((b4 - b3) / (b4 + 6 * b3 - 7.5 * b1 + 1))
+
+    @staticmethod
+    def landsat8_evi(b2, b4, b5):
+        return 2.5 * ((b5 - b4) / (b5 + 6 * b4 - 7.5 * b2 + 1))
+
+    @staticmethod
+    def sentinel2_evi(b2, b4, b8):
+        return 2.5 * ((b8 - b4) / (b8 + 6 * b4 - 7.5 * b2 + 1))
+
