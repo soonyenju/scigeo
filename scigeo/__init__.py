ae9149c95d17341c41c3f20a9b78065174170024
Songyan Zhu <zhusy93@gmail.com>
2025-04-19 07:54:37 +0100

    Update __init__.py

--- scigeo/__init__.py
+++ scigeo/__init__.py
@@ -1,6 +1,2 @@
 # coding: utf-8
-__all__ = ["gdal_wrapper", "geobox", "geoface", "sun"]
-
-# from pysy.scigeo.gdal_wrapper import *
-# from pysy.scigeo.geobox import *
-# from pysy.scigeo.geoface import *
+__all__ = ["geobox", "geoface"]
